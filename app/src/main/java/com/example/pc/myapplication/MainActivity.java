9fc6aeef6a6bec5ec569875c565155cb9cb0237a
pc <1095148656@qq.com>
2018-01-18 16:57:39 +0800

    第一次提交

--- app/src/main/java/com/example/pc/myapplication/MainActivity.java
+++ app/src/main/java/com/example/pc/myapplication/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         String sd="上课的离他很近";
+        
     }
 }
